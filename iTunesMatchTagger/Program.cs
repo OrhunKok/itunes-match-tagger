270a2a460d0f28b7578e00a5017f583bd0b6a841
Orhun Kok <59579802+OrhunKok@users.noreply.github.com>
2025-04-30 17:40:31 -0400

    Update Program.cs
    Implementing TLS fix as suggested in:
    https://www.reddit.com/r/musichoarder/comments/1gid62s/comment/lx0zzy0/

--- iTunesMatchTagger/Program.cs
+++ iTunesMatchTagger/Program.cs
@@ -10,7 +10,8 @@ namespace iTunesMatchTagger
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        {    
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
